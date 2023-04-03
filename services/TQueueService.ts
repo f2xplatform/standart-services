f1ebd7cbb14ea8c21550c0729479037d78372f48
Karina Kushnir <karina.kardash@free2ex.com>
2023-04-03 10:07:38 +0300

    add individual files for service

---
+++ services/TQueueService.ts
@@ -0,0 +1,37 @@
+import { IBaseServiceEnv, TBaseService } from "./TBaseService";
+
+export interface IQueuesServiceEnv extends IBaseServiceEnv {}
+
+export abstract class TQueueService extends TBaseService {
+  protected env_trace: number = this.trace;
+  constructor(env: IQueuesServiceEnv, name: string) {
+    super(env, name);
+  }
+  async init(messageBatch: MessageBatch) {
+    if (this.trace) {
+      await this.traceQueueStart(messageBatch);
+    }
+  }
+  async handleQMessage(message: { id: string; trace: number; message: any }) {
+    this.trace =
+      message.trace > this.env_trace ? message.trace : this.env_trace;
+    this.id = message.id;
+    if (this.trace === 2) {
+      await this.traceMessage(
+        `queue consumer, message: ${JSON.stringify(message.message)}`,
+        "queue_handle"
+      );
+    }
+    return message.message;
+  }
+  private async traceQueueStart(messageBatch: MessageBatch) {
+    await this.traceMessage(
+      `${this.name} queue start, messages - ${messageBatch.messages.length}`,
+      "queue_start"
+    );
+  }
+
+  async traceQueueStop(error?: string) {
+    await this.traceMessage(`${this.name} queue stop`, "queue_stop", error);
+  }
+}
